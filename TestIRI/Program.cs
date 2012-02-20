e453b0e9e6f2062961991987b195e6e910f52e42
Darrel Miller <darrel@tavis.ca>
2012-02-20 13:36:10 -0500

    Initial Commit - All tests working

---
+++ TestIRI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestIRI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reserved3986chars = "-._~";
+            var reserved2396chars = "-_.!~*'()";
+
+            var result = Uri.EscapeUriString(reserved3986chars);
+            var result2 = Uri.EscapeUriString(reserved2396chars);
+
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+            Console.Read();
+        }
+    }
+}
