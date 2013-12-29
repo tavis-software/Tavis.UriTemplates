bab13854a6db8b0145dbd78709753e0013e4c095
Darrel <darrel@tavis.ca>
2013-12-29 11:27:32 -0500

    Some additional tests

--- src/UriTemplateTests/UsageTests.cs
+++ src/UriTemplateTests/UsageTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using Tavis.UriTemplates;
 using Xunit;
@@ -80,6 +81,25 @@ namespace UriTemplateTests
 
 
         [Fact]
+        public void ShouldHandleUriEncoding()
+        {
+            var template = new UriTemplate("http://example.org/sparql{?query}");
+            template.SetParameter("query", "PREFIX dc: <http://purl.org/dc/elements/1.1/> SELECT ?book ?who WHERE { ?book dc:creator ?who }");
+            var uriString = template.Resolve();
+            Assert.Equal("http://example.org/sparql?query=PREFIX%20dc%3A%20%3Chttp%3A%2F%2Fpurl.org%2Fdc%2Felements%2F1.1%2F%3E%20SELECT%20%3Fbook%20%3Fwho%20WHERE%20%7B%20%3Fbook%20dc%3Acreator%20%3Fwho%20%7D", uriString);
+        }
+
+        [Fact]
+        public void ShouldHandleEncodingAParametersThatIsAUriWithAUriAsAParameter()
+        {
+            var template = new UriTemplate("http://example.org/go{?uri}");
+            template.SetParameter("uri", "http://example.org/?uri=http%3A%2F%2Fexample.org%2F");
+            var uriString = template.Resolve();
+            Assert.Equal("http://example.org/go?uri=http%3A%2F%2Fexample.org%2F%3Furi%3Dhttp%253A%252F%252Fexample.org%252F", uriString);
+        }
+
+
+        [Fact]
         public void ShouldThrowWhenExpressionIsNotClosed()
         {
             var result = string.Empty;
@@ -114,6 +134,81 @@ namespace UriTemplateTests
             }
 
             Assert.Equal("Malformed template : http://example.org/foo/{}/baz/", result);
+
+        }
+
+        [Fact]
+        public void Query_param_with_exploded_array()
+        {
+            UriTemplate template = new UriTemplate("/foo/{foo}/baz{?haz*}");
+            template.SetParameter("foo", "1234");
+            template.SetParameter("haz", new string[] { "foo","bar" });
+
+            string uri = template.Resolve();
+
+            Assert.Equal("/foo/1234/baz?haz=foo&haz=bar", uri);
+        }
+
+        [Fact]
+        public void Query_param_with_list_array()
+        {
+            UriTemplate template = new UriTemplate("/foo/{foo}/baz{?haz}");
+            template.SetParameter("foo", "1234");
+            template.SetParameter("haz", new string[] { "foo", "bar" });
+
+            string uri = template.Resolve();
+
+            Assert.Equal("/foo/1234/baz?haz=foo,bar", uri);
+        }
+
+        [Fact]
+        public void Query_param_with_empty_array()
+        {
+            UriTemplate template = new UriTemplate("/foo/{foo}/baz{?haz*}");
+            template.SetParameter("foo", "1234");
+            template.SetParameter("haz", new string[] {});
+
+            string uri = template.Resolve();
+
+            Assert.Equal("/foo/1234/baz",uri);
+        }
+
+        [Fact]
+        public void FactMethodName()
+        {
+            UriTemplate template = new UriTemplate("https://api.github.com/search/code?q={query}{&page,per_page,sort,order}");
+            template.SetParameter("query", "1234");
+            template.SetParameter("per_page", "19");
+            var result = template.Resolve();
+
+        }
+
+
+        [Fact]
+        public void ReservedCharacterExpansion()
+        {
+            UriTemplate template = new UriTemplate("https://foo.com/{?format}");
+            template.SetParameter("format", "application/vnd.foo+xml");
+            
+            var result = template.Resolve();
+
+            Assert.Equal("https://foo.com/?format=application%2Fvnd.foo%2Bxml",result);
+            
+        }
+
+        [Fact]
+        public void PreserveReservedCharacterExpansion()
+        {
+            UriTemplate template = new UriTemplate("https://foo.com/?format={+format}");
+            template.SetParameter("format", "application/vnd.foo+xml");
+
+            var result = template.Resolve();
+
+            Assert.Equal("https://foo.com/?format=application/vnd.foo+xml", result);
+
+            var httpClient = new HttpClient();
+
+            var response = httpClient.GetAsync("http://yahoo.com/foo%2Fbar").Result;
 
         }
 
