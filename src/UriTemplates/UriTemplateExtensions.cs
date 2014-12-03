ca538f24db8c750ffa2d7e779181ef06bc2f07d9
Darrel <darrel@tavis.ca>
2014-12-03 12:41:50 -0500

    Fixed namespace and nuget Title

--- src/UriTemplates/UriTemplateExtensions.cs
+++ src/UriTemplates/UriTemplateExtensions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Tavis.UriTemplates;
-
-namespace Tavis
+﻿namespace Tavis.UriTemplates
 {
     public static class UriTemplateExtensions
     {
