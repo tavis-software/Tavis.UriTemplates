66622a65675266d0869edfa3512d25fc3d7f792d
Darrel <darrel@tavis.ca>
2014-12-16 09:59:16 -0800

    Added more support for making templates out of uris

--- src/UriTemplates/UriTemplateExtensions.cs
+++ src/UriTemplates/UriTemplateExtensions.cs
@@ -1,4 +1,9 @@
-﻿namespace Tavis.UriTemplates
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Tavis.UriTemplates
 {
     public static class UriTemplateExtensions
     {
@@ -23,5 +28,54 @@
 
             return template;
         }
+        public static UriTemplate AddParameters(this UriTemplate uriTemplate, Dictionary<string, object> linkParameters)
+        {
+            if (linkParameters != null)
+            {
+                foreach (var parameter in linkParameters)
+                {
+                    uriTemplate.SetParameter(parameter.Key, parameter.Value);
+                }
+            }
+            return uriTemplate;
+        }
+    }
+
+    public static class UriExtensions
+    {
+        public static UriTemplate MakeTemplate(this Uri uri)
+        {
+            var parameters = uri.GetQueryStringParameters();
+            return MakeTemplate(uri, parameters);
+
+        }
+
+        public static UriTemplate MakeTemplate(this Uri uri, Dictionary<string, object> parameters)
+        {
+            var target = uri.GetComponents(UriComponents.AbsoluteUri
+                                                     & ~UriComponents.Query
+                                                     & ~UriComponents.Fragment, UriFormat.Unescaped);
+            var template = new UriTemplate(target + "{?" + string.Join(",", parameters.Keys.ToArray()) + "}");
+            template.AddParameters(parameters);
+
+            return template;
+        }
+
+        public static Dictionary<string, object> GetQueryStringParameters(this Uri target)
+        {
+            Uri uri = target;
+            var parameters = new Dictionary<string, object>();
+
+            var reg = new Regex(@"([-A-Za-z0-9._~]*)=([^&]*)&?");		// Unreserved characters: http://tools.ietf.org/html/rfc3986#section-2.3
+            foreach (Match m in reg.Matches(uri.Query))
+            {
+                string key = m.Groups[1].Value.ToLowerInvariant();
+                string value = m.Groups[2].Value;
+                parameters.Add(key, value);
+            }
+            return parameters;
+        }
+
+       
     }
 }
