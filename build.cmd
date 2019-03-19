356ff9712fa89abbde3e14968e5de44908f0424a
Daniel Crenna <daniel.crenna@gmail.com>
2019-03-18 23:10:38 -0400

    polish the build.cmd

--- build.cmd
+++ build.cmd
@@ -4,5 +4,5 @@ if "%config%" == "" (
    set config=debug
 )
 md artifacts
-dotnet build --configuration %config% --verbosity=normal /m /v:M /fl /flp:LogFile=msbuild.log; /nr:false
-dotnet pack --configuration %config% --output artifacts
+dotnet build --configuration %config% --verbosity normal /m /v:M /fl /flp:LogFile=msbuild.log; /nr:false
+dotnet pack --no-build --configuration %config% --output %cd%\artifacts
